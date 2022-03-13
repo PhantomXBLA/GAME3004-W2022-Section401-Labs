07b58a62fa1e71ace8d2734919e1d2cd746ed421
Tyler <iamtyler@live.ca>
2022-03-12 19:56:15 -0500

    Added enemy controller and added player movement

--- GAME3004-W2022-Lab2/Assets/Scripts/PlayerBehaviour.cs
+++ GAME3004-W2022-Lab2/Assets/Scripts/PlayerBehaviour.cs
@@ -24,7 +24,27 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundRadius, groundMask);   
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundRadius, groundMask);
+
+        if (isGrounded && velocity.y < 0.0f)
+        {
+            velocity.y = -2.0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+        }
+
+        // apply gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 
     private void OnDrawGizmos()

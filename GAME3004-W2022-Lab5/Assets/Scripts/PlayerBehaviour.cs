6286071fa81ead97417e4a2c83d98f110c1ad544
Tyler <iamtyler@live.ca>
2022-03-12 23:08:13 -0400

    Lab 5 project files added

---
+++ GAME3004-W2022-Lab5/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public CharacterController controller;
+    public float gravity = -30f;
+    public float speed = 10f;
+    public float jumpHeight = 3f;
+    public Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundRadius = 0.4f;
+    public LayerMask groundMask;
+    public bool isGrounded;
+
+    public UIController uiController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundRadius, groundMask);
+
+        if (isGrounded && velocity.y < 0.0f)
+        {
+            velocity.y = -2.0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+        }
+
+        // apply gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hazard"))
+        {
+            uiController.TakeDamage(5);
+        }
+    }
+}
+
